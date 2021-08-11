1514dbf458392200911116f110fff3e6f75fea3a
aravindhkm <aravindhkathiresan@gmail.com>
2021-08-11 09:00:26 +0530

    first commit

---
+++ src/config/constants/farmToken.ts
@@ -0,0 +1,53 @@
+const farmToken = {
+    bnb: {
+      symbol: 'BNB',
+      projectLink: 'https://www.binance.com/',
+    },
+    cake: {
+      symbol: 'CAKE',
+      address: {
+        56: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+        97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+      },
+      decimals: 18,
+      projectLink: 'https://pancakeswap.finance/',
+    },
+    GeeksA : {
+      symbol: 'GeeksA',
+      address: {
+        56: '',
+        97: '0xe58C72A03Bd0E57a1384706E09BE373931E5C846',
+      },
+      decimals: 18,
+      projectLink: 'https://tranchess.com/',
+    },
+    GeeksB: {
+      symbol: 'GeeksB',
+      address: {
+        56: '',
+        97: '0x9C82D6dC6E35eB6420f0C463f05dBb3216598EFA',
+      },
+      decimals: 18,
+      projectLink: 'https://titanswap.org',
+    },
+    harmony: {
+      symbol: 'ONE',
+      address: {
+        56: '0x03fF0ff224f904be3118461335064bB48Df47938',
+        97: '',
+      },
+      decimals: 18,
+      projectLink: 'https://www.harmony.one/',
+    },
+    mask: {
+      symbol: 'MASK',
+      address: {
+        56: '0x2eD9a5C8C13b93955103B9a7C167B67Ef4d568a3',
+        97: '',
+      },
+      decimals: 18,
+      projectLink: 'https://mask.io/',
+    }
+  }
+  
+  export default farmToken
